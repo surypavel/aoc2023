b26a4aa05b01e4cc51d84093ea5ca8cca3a6a97f
Pavel Surý <pavel.sury@rossum.ai>
2023-12-09 10:02:59 +0100

    09

---
+++ 09b.exs
@@ -0,0 +1,41 @@
+{:ok, str} = File.read("09.txt")
+
+defmodule AOC do
+  def load_numbers(input) do
+    Regex.scan(~r/-?\d+/, input)
+    |> List.flatten()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def diff_sequences(seq) do
+    Enum.zip([0 | seq], seq) |> Enum.map(fn {a, b} -> b - a end) |> Enum.drop(1)
+  end
+
+  def all_diff_sequences(seq) do
+    next_sequence = diff_sequences(seq)
+
+    if Enum.all?(next_sequence, fn el -> el == 0 end) do
+      [[0 | next_sequence]]
+    else
+      all_diff_sequences(next_sequence) ++ [next_sequence]
+    end
+  end
+
+  def backtrack([seq]) do
+    Enum.at(seq, 0)
+  end
+
+  def backtrack([seq1, seq2 | rest]) do
+    new = -Enum.at(seq1, 0) + Enum.at(seq2, 0)
+    backtrack([[new | seq2] | rest])
+  end
+
+  def predict_sequence(str) do
+    str
+    |> AOC.load_numbers()
+    |> (fn x -> AOC.all_diff_sequences(x) ++ [x] end).()
+    |> AOC.backtrack()
+  end
+end
+
+str |> String.split("\n") |> Enum.map(&AOC.predict_sequence/1) |> Enum.sum() |> IO.puts()
