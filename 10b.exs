74dc7dca4799553045a6b2147a185b72a23779c5
Pavel Surý <pavel.sury@rossum.ai>
2023-12-10 22:53:14 +0100

    10 - shorter version

--- 10b.exs
+++ 10b.exs
@@ -13,47 +13,33 @@ defmodule AOC do
     end)
   end
 
-  def determine_position(flip_op) do
+  def determine_position(flip_left, flip_op) do
     case flip_op do
-      {false, false, "."} -> false
-      {true, true, "."} -> true
-      {true, true, "F"} -> false
-      {false, false, "F"} -> true
-      {true, true, "J"} -> true
-      {false, false, "J"} -> false
-      {true, false, "7"} -> false
-      {false, true, "7"} -> true
-      {false, true, "-"} -> false
-      {true, false, "-"} -> true
-      {false, true, "L"} -> false
-      {true, false, "L"} -> true
-      {false, true, "|"} -> true
-      {true, false, "|"} -> false
+      "." -> flip_left
+      "F" -> !flip_left
+      "J" -> flip_left
+      "7" -> !flip_left
+      "-" -> flip_left
+      "L" -> flip_left
+      "|" -> !flip_left
     end
   end
 
-  def flood(map, index, flips) do
-    0..index
-    |> Enum.map(fn i -> {i, index - i} end)
-    |> Enum.map(fn point ->
-      {x, y} = point
-      flip_x = Map.get(flips, {x - 1, y}, false)
-      flip_y = Map.get(flips, {x, y - 1}, false)
-      op = Map.get(map, point, ".")
-      new_flip = AOC.determine_position({flip_x, flip_y, op})
-      {point, new_flip}
-    end)
-    |> Map.new()
-    |> Map.merge(flips)
+  def calc_point(map, point, flips) do
+    { x, y } = point
+    flip_x = Map.get(flips, {x - 1, y}, false)
+    op = Map.get(map, point, ".")
+    new_flip = AOC.determine_position(flip_x, op)
+    Map.put(flips, point, new_flip)
   end
 end
 
 map_list = str |> AOC.load_map()
 map = map_list |> Map.new()
+map_keys = map |> Map.keys() |> Enum.sort_by(fn { x, y} -> { y, x} end)
 dots = map_list |> Enum.filter(fn {_, v} -> v == "." end) |> Enum.map(fn {k, _} -> k end)
-{size_x, size_y} = map |> Map.keys() |> Enum.max()
 
-result = Enum.reduce(0..(size_x + size_y + 1), Map.new(), &AOC.flood(map, &1, &2))
+result = Enum.reduce(map_keys, Map.new(), &AOC.calc_point(map, &1, &2))
 
 dots
 |> Enum.filter(fn dot -> Map.get(result, dot) == true end)
